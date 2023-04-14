dea9ebac634f949b1078a88f83481aace96efb7b
achahine78 <achahine78@gmail.com>
2023-04-14 19:19:35 +0400

    feat: add tabs to switch between screens in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { Box, Tab, Tabs } from "@mui/material";
+import { useState } from "react";
+
 function App() {
-  return <div className="App">hello world</div>;
+  const [value, setValue] = useState(0);
+
+  const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className="App">
+      <Box sx={{ width: "100%" }}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+          >
+            <Tab label="Birthday Selection" />
+            <Tab label="Liked Birthdays" />
+          </Tabs>
+        </Box>
+        { value === 0 && '0'}
+        { value === 1 && '1'}
+      </Box>
+    </div>
+  );
 }
 
 export default App;
